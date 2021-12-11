65876a6d13a504a3e7daf3167ef5209d9902e984
Anis Rahel <58486759+Hakueiryu@users.noreply.github.com>
2021-12-11 07:04:23 +0100

    Uploaded Day 11

---
+++ 2021/Day_11/Part_1.py
@@ -0,0 +1,49 @@
+matrix = []
+flashed = []
+flashes = 0
+
+directions = [
+    (0, 1),
+    (0, -1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+    (-1, -1),
+    (-1, 0),
+    (-1, 1)
+]
+
+def get_adjacent(x,y):
+    return [((x+dx), (y+dy)) for dx, dy in directions if 0 <= x+dx <= len(matrix[0]) - 1 and 0 <= y+dy <= len(matrix) - 1]
+
+def simulate_step():
+    global matrix
+
+    for x in range(len(matrix[0])):
+        for y in range(len(matrix)):
+            increase_step(x,y)
+
+def increase_step(x,y):
+    global flashes
+    global matrix
+    global flashed
+
+    if not flashed[x][y]:
+        matrix[x][y] += 1
+        if matrix[x][y] > 9:
+            matrix[x][y] = 0
+            flashed[x][y] = 1
+            flashes += 1
+            for a, b in get_adjacent(x, y):
+                increase_step(a, b)
+
+
+if __name__ == '__main__':
+    file = open('input.txt', 'r').read().strip().split('\n')
+    matrix = [list(map(int, list(x))) for x in file]
+
+    for _ in range(100):
+        flashed = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0] for _ in range(len(matrix))]
+        simulate_step()
+
+    print(f'Part 1: {flashes}')
